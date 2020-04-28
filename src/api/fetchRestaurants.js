7bc8ea326a03d5dfaddfe4d2e735920e2f44e1df
robertlovin23 <robert.a.lovin@gmail.com>
2020-04-28 12:13:38 -0500

    Modal Edits, TODO: refactor custom hooks

--- src/api/fetchRestaurants.js
+++ src/api/fetchRestaurants.js
@@ -1,24 +1,31 @@
-// import { useState,useEffect } from 'react'
+import { useState,useEffect } from 'react'
 
-// const FetchRestaurants = () => {
-//     const [restaurant,getRestaurant] = useState([])
+const FetchRestaurants = async () => {
+    const [selectedRestaurant,getSelectedRestaurant] = useState(null)
+    const [restaurant,getRestaurant] = useState([])
 
-//         //Use Fetch to obtain response, convert to JSON and then set the state via getRestaurant
-//         const fetchData = async () => {
-//             const response = await fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants',{
-//                 headers: {
-//                     'Authorization': 'Api-Key q3MNxtfep8Gt',
-//                 },
-//             }).then((response) => response.json())
-//             getRestaurant(response)
-//         }
+    //Use Fetch to obtain response and then return the JSON response
+        const response = await fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants',{
+            headers: {
+                'Authorization': 'Api-Key q3MNxtfep8Gt',
+            },
+        }).then((response) => response.json())
 
-//         //Mounts fetch data to the component, I use an array at the end of the argument so that it prevents and infinite loop
-//         useEffect(() => {
-//             fetchData();
-//         }, [])
+        //Uses a search query and filter to be able to look through each restaurants name,city of physical location, and restaurant genre   
+        response.sort((a,b) => {
+            if(a.name > b.name){
+                return 1
+            } else if(a.name < b.name) {
+                return -1
+            }
+            return;
+        })
+        //If Search has no results than return the original list else give them the filtered list
+        getRestaurant(response)
+        getSelectedRestaurant(response[0])
+        // setCurrentRows(1)
 
-//         return restaurant;
-//     }
+    return restaurant,selectedRestaurant
+}
 
-// export default FetchRestaurants;
+export default FetchRestaurants;
---
+++ src/components/TableRows.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react'
+import RestaurantDetails from './RestaurantDetails'
+
+const TableRows = (props) => {
+    const [modalOpen,setModalOpen] = useState(false)
+
+    const openModal = () => {
+        setModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setModalOpen(false)
+    }
+
+    const renderModal = () => {
+        if(modalOpen){
+           return <RestaurantDetails restaurant={selectedRestaurant} closeModal={closeModal}/>
+        }
+    }
+
+    const { restaurant,currentRestaurants,selectRestaurant,selectedRestaurant } = props
+
+    if(!currentRestaurants.length){
+        return(
+            <div style={{margin:"0 auto"}}>No Results</div>
+        )
+    }
+    const response = restaurant.length > 0 ? currentRestaurants.map(rest => {
+        return(
+
+            <tr rest={rest.id} onClick={() => {selectRestaurant(rest)}}>
+                <td data-label="Name" onClick={openModal}>{rest.name}</td>
+                <td data-label="City">{rest.city}</td>
+                <td data-label="State">{rest.state}</td>
+                <td data-label="Number">{rest.telephone}</td>
+                <td data-label="Genre">{rest.genre}</td>
+            </tr>
+        
+        )
+    }) : <tr style={{alignContent:"center"}}>No results found.</tr>
+    return(
+    <React.Fragment>
+        <table class="ui celled table">  
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Phone Number</th>
+                    <th>Genre</th>
+                </tr>
+            </thead>
+            <tbody>
+                {response}
+            </tbody>
+
+        </table>
+            {renderModal()}
+    </React.Fragment>
+    )
+}
+
+
+export default TableRows
